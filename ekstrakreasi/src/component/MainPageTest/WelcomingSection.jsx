aba129b60fd7b329d06b0a2007bda808a9656b78
Alfonso Mugi <mugialfonso09@gmail.com>
2024-12-23 11:15:57 +0700

    perubahan nama dengan state local

--- ekstrakreasi/src/component/MainPageTest/WelcomingSection.jsx
+++ ekstrakreasi/src/component/MainPageTest/WelcomingSection.jsx
@@ -1,12 +1,12 @@
 
-function WelcomingSection ({onNext}) {
+function WelcomingSection ({nama, onNext}) {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen ">
             <div className="bg-white rounded-lg shadow-2xl p-8 md:p-20 w-full max-w-md md:max-w-[744px] h-auto md:h-[426px]" >
                 <div className="p-8">
                     <h2 className=" text-lg md:text-2xl font-bold text-center text-[#67C6E3] mb-6">
-                    Hi, Anthony!
+                    Hi, {nama}!
                     </h2>
                     <p className="text-sm md:text-base font-bold text-center text-[#67C6E3] mb-6">
                         Siap untuk menemukan kegiatan yang pas untukmu? 
