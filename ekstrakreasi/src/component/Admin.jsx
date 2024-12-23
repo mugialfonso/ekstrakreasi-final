f7cdbe0ccd6061613cd170cddc06525308c8ab10
Alfonso Mugi <mugialfonso09@gmail.com>
2024-12-23 11:10:00 +0700

    Fix Layout Tabel daftar profil siswa

--- ekstrakreasi/src/component/Admin.jsx
+++ ekstrakreasi/src/component/Admin.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function Admin() {
   const [students, setStudents] = useState([]);
-  
+
   // GET data
   useEffect(() => {
     axios
@@ -37,9 +37,7 @@ function Admin() {
       axios
         .patch(`http://localhost:4000/users/${id}`, { nama: updatedName })
         .then((response) => {
-          const updatedStudents = students.map((student) =>
-            student.id === id ? { ...student, nama: updatedName } : student
-          );
+          const updatedStudents = students.map((student) => (student.id === id ? { ...student, nama: updatedName } : student));
           setStudents(updatedStudents);
           alert("User updated successfully!");
         })
@@ -52,47 +50,38 @@ function Admin() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-5">Daftar Nama Siswa</h1>
-      <table className="table-auto w-full bg-white shadow-md rounded">
-        <thead>
-          <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-            <th className="py-3 px-6 text-left">ID</th>
-            <th className="py-3 px-6 text-left">Nama</th>
-            <th className="py-3 px-6 text-left">gender</th>
-            <th className="py-3 px-6 text-left">Kelas</th>
-            <th className="py-3 px-6 text-center"></th>
-          </tr>
-        </thead>
-        <tbody className="text-gray-600 text-sm font-light">
-          {students.map((student) => (
-            <tr
-              key={student.id}
-              className="border-b border-gray-200 hover:bg-gray-100"
-            >
-              <td className="py-3 px-6 text-left whitespace-nowrap">
-                {student.id}
-              </td>
-              <td className="py-3 px-6 text-left">{student.nama}</td>
-              <td className="py-3 px-6 text-left">{student.gender}</td>
-              <td className="py-3 px-6 text-left">{student.kelas}</td>
-              <td className="py-3 px-6 text-center">
-                <button
-                  onClick={() => handleUpdate(student.id)}
-                  className="bg-blue-500 text-white py-1 px-3 rounded mr-2 hover:bg-blue-600"
-                >
-                  Update
-                </button>
-                <button
-                  onClick={() => handleDelete(student.id)}
-                  className="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600"
-                >
-                  Delete
-                </button>
-              </td>
+      <h1 className="text-2xl font-bold mb-5">Daftar Siswa</h1>
+      <div className="w-full max-w-4xl overflow-auto max-h-[500px]">
+        <table className="table-auto w-full bg-white shadow-md rounded">
+          <thead>
+            <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+              <th className="py-3 px-6 text-left">ID</th>
+              <th className="py-3 px-6 text-left">Nama</th>
+              <th className="py-3 px-6 text-left">Gender</th>
+              <th className="py-3 px-6 text-left">Kelas</th>
+              <th className="py-3 px-6 text-center"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="text-gray-600 text-sm font-light">
+            {students.map((student) => (
+              <tr key={student.id} className="border-b border-gray-200 hover:bg-gray-100">
+                <td className="py-3 px-6 text-left whitespace-nowrap">{student.id}</td>
+                <td className="py-3 px-6 text-left">{student.nama}</td>
+                <td className="py-3 px-6 text-left">{student.gender}</td>
+                <td className="py-3 px-6 text-left">{student.kelas}</td>
+                <td className="py-3 px-6 text-center">
+                  <button onClick={() => handleUpdate(student.id)} className="bg-blue-500 text-white py-1 px-3 rounded mr-2 hover:bg-blue-600">
+                    Update
+                  </button>
+                  <button onClick={() => handleDelete(student.id)} className="bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
